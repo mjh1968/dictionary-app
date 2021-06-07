203e1c5afee39ed4e892683e3b0f547f6bb4ddd4
mjh1968 <mjbcbh@gmail.com>
2021-06-07 16:04:35 +0100

    styling app and app-header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>DICTIONARY</h1>
-      </header>
+    <div className="container">
+      <div className="App">
+        <header className="App-header">
+          <h1>DICTIONARY</h1>
+        </header>
+      </div>
     </div>
   );
 }
