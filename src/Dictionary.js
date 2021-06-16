a9aeab1b39ef1412b063600cca463d3c1aaeb61b
mjh1968 <mjbcbh@gmail.com>
2021-06-16 16:48:24 +0100

    DICTIONARY.JS

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
+import "./Dictionary.css";
 
 export default function Dictionary() {
   let [word, setWord] = useState(null);
+
   function search(event) {
     event.preventDefault();
-    return null;
+    return alert("ola");
   }
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
-        <input type="search" className="input">
-          {" "}
-        </input>
+        <input type="search" className="input"></input>
       </form>
     </div>
   );
