9317a94348c5cd243f6b4b4c9cca87f1db2ae00a
mjh1968 <mjbcbh@gmail.com>
2021-07-13 17:44:19 +0100

    add DateF css and js

--- src/DateF.js
+++ src/DateF.js
@@ -1,3 +1,39 @@
 import React from "react";
+import "./DateF.css";
 
-export default function DateF() {}
+export default function DateF() {
+  let myCurrentDate = new Date();
+  let date = myCurrentDate.getDate();
+  let month = myCurrentDate.getMonth() + 1;
+  let year = myCurrentDate.getFullYear();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[myCurrentDate.getDay()];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let monthName = months[myCurrentDate.getMonth()];
+  return (
+    <div className="dateFormatted">
+      <span class="text-uppercase">{day}</span>, {date} {monthName} {year}
+    </div>
+  );
+}
